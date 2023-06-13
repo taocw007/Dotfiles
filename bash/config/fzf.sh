f83c0c8bcde92f852e33d9cced57b56c8769303a
ooohpi <taochangwan@qq.com>
2023-06-13 21:24:28 +0800

    [bash]: add more exclude dirs for fzf

--- bash/config/fzf.sh
+++ bash/config/fzf.sh
@@ -1,5 +1,15 @@
-[ -x $(which fzf 2>/dev/null) ] || return
-source /usr/share/fzf/key-bindings.bash
-source /usr/share/bash-completion/completions/fzf
+[ -x "$(which fzf 2>/dev/null)" ] || return
+source '/usr/share/fzf/key-bindings.bash'
+source '/usr/share/fzf/completion.bash'
 
-export FZF_COMPLETION_TRIGGER='*'
+EXCLUDES="--exclude={.git,.idea,node_modules,__pycache__,.cache}"
+
+export FZF_COMPLETION_TRIGGER=''
+export FZF_DEFAULT_COMMAND="fd ${EXCLUDES} --type f"
+_fzf_compgen_path() {
+  fd --hidden --follow ${EXCLUDES} . "$1"
+}
+
+_fzf_compgen_dir() {
+  fd --type d --hidden --follow ${EXCLUDES} . "$1"
+}
