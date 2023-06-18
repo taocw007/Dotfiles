aa25b051257978d7f97059af40af3bc7da0a50d2
ooohpi <taochangwan@qq.com>
2023-06-18 21:42:55 +0800

    [bash]: export more environment variables

--- bash/config/envs.sh
+++ bash/config/envs.sh
@@ -1,8 +1,17 @@
-export PATH="${PATH}:$HOME/workspace/scripts"
-export EDITOR='nvim'
-export PAGER='less'
-export wallpapers="$HOME/Pictures/wallpapers"
+#!/usr/bin/env sh
+
 export HISTCONTROL=ignoredups
-export GTK_IM_MODULE=fcitx
-export QT_IM_MODULE=fcitx
-export XMODIFIERS=@im=fcitx
+
+export PATH="${PATH}:/opt/cuda/bin:/usr/local/texlive/2023/bin/x86_64-linux"
+export PAGER='less'
+[ -x "$(which nvim)" ] && export EDITOR='nvim'
+export MANPATH="/usr/man:/usr/share/man:/usr/local/man:/usr/local/share/man:/usr/X11R6/man:/opt/man:/var/lib/snapd/snap/man:/usr/local/texlive/2023/texmf-dist/doc/man"
+export INFOPATH="/usr/share/info:/usr/local/texlive/2023/texmf-dist/doc/info"
+
+export wallpapers="$HOME/Pictures/wallpapers"
+[ -x "$(which fcitx5 2>/dev/null)" ] &&\
+{
+    export GTK_IM_MODULE=fcitx
+    export QT_IM_MODULE=fcitx
+    export XMODIFIERS=@im=fcitx
+}
