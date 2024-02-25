a2590355db8e0cec403ce0d59e606ed64cfcec01
taocw <taocw@foxmail.com>
2024-02-25 20:02:34 +0800

    [bash]: use bash-completion

--- bash/config/settings.sh
+++ bash/config/settings.sh
@@ -2,6 +2,10 @@ HISTSIZE=10000
 HISTFILESIZE=20000
 HISTFILE=$HOME/.cache/bashhistory
 
+# Use bash-completion, if available
+[[ $PS1 && -f /usr/share/bash-completion/bash_completion ]] &&
+	. /usr/share/bash-completion/bash_completion
+
 bind -m vi-insert 'Control-l: clear-screen'
 
 shopt -s autocd
