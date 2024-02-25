f39fbd7a2ca0da52e76bbc0d28fb05e9496bdacf
taocw <taocw@foxmail.com>
2024-02-25 21:23:36 +0800

    [bash]: fix

--- bash/config/settings.sh
+++ bash/config/settings.sh
@@ -3,8 +3,8 @@ HISTFILESIZE=20000
 HISTFILE=$HOME/.cache/bashhistory
 
 # Use bash-completion, if available
-[[ $PS1 && -f /usr/share/bash-completion/bash_completion ]] &&
-	. /usr/share/bash-completion/bash_completion
+#[[ $PS1 && -f /usr/share/bash-completion/bash_completion ]] &&
+#	. /usr/share/bash-completion/bash_completion
 
 bind -m vi-insert 'Control-l: clear-screen'
 
