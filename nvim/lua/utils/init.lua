8f811b5d021d407c3a95c2c6fc7441c4bb90d602
taocw <taocw@foxmail.com>
2024-03-23 20:13:53 +0800

    [neovim]: from LayzVim

---
+++ nvim/lua/utils/init.lua
@@ -0,0 +1,22 @@
+local M = {}
+
+-- Wrapper around vim.keymap.set that will
+-- not create a keymap if a lazy key handler exists.
+-- It will also set `silent` to true by default.
+function M.safe_keymap_set(mode, lhs, rhs, opts)
+  local keys = require("lazy.core.handler").handlers.keys
+  local modes = type(mode) == "string" and { mode } or mode
+
+  modes = vim.tbl_filter(function(m)
+    return not (keys.have and keys:have(lhs, m))
+  end, modes)
+
+  -- do not create the keymap if a lazy keys handler exists
+  if #modes > 0 then
+    opts = opts or {}
+    opts.silent = opts.silent ~= false
+    vim.keymap.set(modes, lhs, rhs, opts)
+  end
+end
+
+return M
