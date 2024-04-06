03d19846ada5dd2e9472f498a2e79e45e7e5ecab
taocw <taocw@foxmail.com>
2024-04-06 18:26:54 +0800

    [neovim]: keymaps

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -36,8 +36,8 @@ map("n", "<leader>bb", "<cmd>e #<cr>", { desc = "Switch to Other Buffer" })
 -- Resize window using <ctrl> arrow keys
 map("n", "<C-Up>", "<cmd>resize +2<cr>", { desc = "Increase window height" })
 map("n", "<C-Down>", "<cmd>resize -2<cr>", { desc = "Decrease window height" })
-map("n", "<C-Left>", "<cmd>vertical resize -2<cr>", { desc = "Decrease window width" })
-map("n", "<C-Right>", "<cmd>vertical resize +2<cr>", { desc = "Increase window width" })
+map("n", "<C-Left>", "<cmd>vertical resize +2<cr>", { desc = "Decrease window width" })
+map("n", "<C-Right>", "<cmd>vertical resize -2<cr>", { desc = "Increase window width" })
 
 -- lazy
 map("n", "<leader>l", "<cmd>Lazy<cr>", { desc = "Lazy" })
