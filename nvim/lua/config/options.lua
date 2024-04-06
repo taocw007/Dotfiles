138563512e69a89bf5212b3b62c12ff0595948b7
taocw <taocw@foxmail.com>
2024-04-06 18:26:24 +0800

    [neovim]: options

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -11,6 +11,8 @@ end
 opt.autowrite = true
 opt.number = true
 opt.cursorline = true
+opt.smartindent = true
+opt.smartcase = true
 opt.expandtab = true
 opt.tabstop = 2
 opt.shiftwidth = 2
@@ -19,5 +21,15 @@ opt.splitbelow = true
 opt.splitright = true
 opt.timeout = true
 opt.timeoutlen = 500
-
+opt.wildmode = "longest,list"
+opt.colorcolumn = "80"
 opt.statuscolumn = [[%=%l %s]]
+opt.undofile = true
+opt.undolevels = 10000
+opt.confirm = true
+opt.ignorecase = true
+opt.laststatus = 0
+opt.list = true
+opt.shiftround = true
+opt.signcolumn = "yes"
+opt.wrap = false
