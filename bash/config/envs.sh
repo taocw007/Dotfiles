a3b2cb0708502c78e5fe089eb1e81e7ad1e92c3d
taocw <taocw@foxmail.com>
2024-08-07 21:19:50 +0800

    [bash]: bad environment variables

--- bash/config/envs.sh
+++ bash/config/envs.sh
@@ -3,10 +3,7 @@
 export HISTCONTROL=ignoredups
 
 export PATH="$HOME/.local/bin:${PATH}:/opt/cuda/bin:/usr/local/texlive/2023/bin/x86_64-linux"
-export PAGER='less'
 [ -x "$(which nvim)" ] && export EDITOR='nvim'
-export MANPATH="/usr/man:/usr/share/man:/usr/local/man:/usr/local/share/man:/usr/X11R6/man:/opt/man:/var/lib/snapd/snap/man:/usr/local/texlive/2023/texmf-dist/doc/man"
-export INFOPATH="/usr/share/info:/usr/local/texlive/2023/texmf-dist/doc/info"
 
 export wallpapers="$HOME/Pictures/wallpapers"
 export DWM="$HOME/workspace/dwm"
