d6f1ae3ef1161ea380c619d4a6f259199ccd1594
taocw <taocw@foxmail.com>
2024-10-05 21:45:42 +0800

    [neovim]: neovim supports it by default

--- nvim/lua/config/autocmds.lua
+++ nvim/lua/config/autocmds.lua
@@ -2,24 +2,6 @@ local function augroup(name)
   return vim.api.nvim_create_augroup("taovim_" .. name, { clear = true })
 end
 
--- go to last loc when opening a buffer
-vim.api.nvim_create_autocmd("BufReadPost", {
-  group = augroup("last_loc"),
-  callback = function(event)
-    local exclude = { "gitcommit" }
-    local buf = event.buf
-    if vim.tbl_contains(exclude, vim.bo[buf].filetype) or vim.b[buf].taovim_last_loc then
-      return
-    end
-    vim.b[buf].taovim_last_loc = true
-    local mark = vim.api.nvim_buf_get_mark(buf, '"')
-    local lcount = vim.api.nvim_buf_line_count(buf)
-    if mark[1] > 0 and mark[1] <= lcount then
-      pcall(vim.api.nvim_win_set_cursor, 0, mark)
-    end
-  end,
-})
-
 -- close some filetypes with <q>
 vim.api.nvim_create_autocmd("FileType", {
   group = augroup("close_with_q"),
