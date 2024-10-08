04c93ac9647826ab3e6f996cc43a6bb3d84e55f6
taocw <taocw@foxmail.com>
2024-10-09 00:02:18 +0800

    [emacs]: try again

--- emacs/init.el
+++ emacs/init.el
@@ -1,1 +1,29 @@
 (provide 'init)
+
+(setq confirm-kill-emacs #'yes-or-no-p)
+(setq inhibit-startup-message t)
+
+(set-frame-font "Hack Nerd Font Mono 16")
+(global-display-line-numbers-mode)
+(tool-bar-mode -1)
+(menu-bar-mode -1)
+(when (display-graphic-p) (toggle-scroll-bar -1))
+
+
+
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-enabled-themes '(gruber-darker))
+ '(custom-safe-themes
+   '("603a831e0f2e466480cdc633ba37a0b1ae3c3e9a4e90183833bc4def3421a961" "2ff9ac386eac4dffd77a33e93b0c8236bb376c5a5df62e36d4bfa821d56e4e20" "e13beeb34b932f309fb2c360a04a460821ca99fe58f69e65557d6c1b10ba18c7" "d80952c58cf1b06d936b1392c38230b74ae1a2a6729594770762dc0779ac66b7" default))
+ '(package-selected-packages '(dracula-theme gruber-darker-theme gruvbox-theme)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
